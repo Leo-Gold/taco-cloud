f7292cfff619fd76fa70485e5d9ae9a70c6ffdec
Maxim Borodkin <m915305991@gmail.com>
2022-10-13 16:21:17 +0300

    refactoring homeController

--- src/main/java/tacos/Controller/HomeController.java
+++ src/main/java/tacos/Controller/HomeController.java
@@ -1,12 +1,13 @@
 package tacos.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Controller
-public class HomeController {
-	@GetMapping("/")
-	public String home() {
-		return "home";
-	}
+@Configuration
+public class HomeController implements WebMvcConfigurer{
+	@Override
+	 public void addViewControllers(ViewControllerRegistry registry) {
+	 registry.addViewController("/").setViewName("home");
+	 }
 }
